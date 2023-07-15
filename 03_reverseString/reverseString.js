bbc819928d05da2351e67d1151bb42f9e9e9885d
David Olona <damilear@outlook.com>
2023-07-14 22:18:23 -0400

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // var for result
+    let str = '';
+    let lengthS = string.length
 
+    // iterate backwords through string and append it to result
+    for (i = lengthS-1; i >= 0; i--) {
+        str = str + string[i];
+    }
+    return str;
 };
 
 // Do not edit below this line
