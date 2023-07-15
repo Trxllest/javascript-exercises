ef8f99b984a51687d40e6e979bb0772255252c9e
David Olona <damilear@outlook.com>
2023-07-14 23:19:37 -0400

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...nums) {
+    const result = array;
 
+    // for each param in nums check the array for its existance and remove it
+    for (let num of nums) {
+        let len = result.length;
+        for (let i = 0; i < len; i++) {
+            if (result[i] === num) {
+                result.splice(i, 1);
+            }
+        }
+    }
+    
+    return result;
 };
 
 // Do not edit below this line
