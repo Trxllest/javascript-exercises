e3756ce12f894437ad1f0879906a831be28a507f
David Olona <damilear@outlook.com>
2023-07-16 01:09:34 -0400

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeFahrenheit) {
+  // conversion
+  let degreeCelsius = (degreeFahrenheit - 32) * (5 / 9);
+
+  // rounding
+  degreeCelsius = Math.round(degreeCelsius * 10) / 10;
+
+  return degreeCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeCelsius) {
+  // conversion 
+  let degreeFahrenheit = (degreeCelsius * (9 / 5) + 32);
+
+  // rounding
+  degreeFahrenheit = Math.round(degreeFahrenheit * 10) / 10;
+
+  return degreeFahrenheit;
 };
 
 // Do not edit below this line
