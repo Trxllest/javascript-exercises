72c379f4fd758e87e29d3e8f50232f5e9cf055b3
David Olona <damilear@outlook.com>
2023-07-16 14:29:45 -0400

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let pattern = /[A-z0-9]/g;
+    let result = string.match(pattern);
+    result = result.join('').toLowerCase();
+    let start = 0;
+    let end = result.length - 1;
+    console.log(result);
+    while (start <= end) {
+        if (result[start] !== result[end]) {
+            console.log(start);
+            console.log(end);
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
 };
 
 // Do not edit below this line
