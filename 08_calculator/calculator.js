d460a6793310a7d92f09f7400550f79557e99e80
David Olona <damilear@outlook.com>
2023-07-16 01:30:45 -0400

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(int1, int2) {
+	return int1 + int2;
 };
 
-const subtract = function() {
-	
+const subtract = function(int1, int2) {
+	return int1 - int2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sumArray = 0;
+  for (let index in array) {
+    sumArray += array[index];
+  }
+  return sumArray;
 };
 
-const multiply = function() {
-
+const multiply = function(...numbers) {
+  let productNumbers = 1;
+  for (let index in numbers) {
+    productNumbers *= numbers[index];
+  }
+  return productNumbers;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	let solution = base;
+  for (let i = 1; i < power; i++) {
+    solution *= base;
+  }
+  return solution;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let solution = 1;
+  for (let i = 1; i <= number; i++) {
+    solution *= i;
+  }
+  return solution;
 };
 
 // Do not edit below this line
