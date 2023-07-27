c10a333da4cca27f8ec4f9ba72cbef2db69a92d8
David Olona <damilear@outlook.com>
2023-07-27 16:55:33 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let newPeople = people.map(person => {
+        if(!person.yearOfDeath) {
+            person.yearOfDeath = (new Date()).getFullYear();
+        }
+        person.age = person.yearOfDeath - person.yearOfBirth;
+        return person;
+    })
+    console.log(newPeople);
 
+    let sortedPeople = newPeople.sort( (person1, person2) => person1.age > person2.age ? -1 : 1);
+    console.log(sortedPeople[0]);
+    return sortedPeople[0];
 };
 
 // Do not edit below this line
